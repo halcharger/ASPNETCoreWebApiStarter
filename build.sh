6e3783de0d30f2eaa4d225180a5e07acfd7065e8
Seth Jones <SethJones@live.co.za>
2017-04-13 20:28:45 +0200

    Converted to VS 2017 and .csproj

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 dotnet restore 
-dotnet build **/project.json
-dotnet test ./src/StarterProject.UnitTests/project.json
-dotnet test ./src/StarterProject.InMemoryUnitTests/project.json
-dotnet test ./src/StarterProject.IntegrationTests/project.json
+dotnet build ./src/StarterProject.UnitTests/StarterProject.UnitTests.csproj
+dotnet build ./src/StarterProject.InMemoryUnitTests/StarterProject.InMemoryUnitTests.csproj
+dotnet build ./src/StarterProject.IntegrationTests/StarterProject.IntegrationTests.csproj
+dotnet build ./src/StarterProject.WebApi/StarterProject.WebApi.csproj
+dotnet test ./src/StarterProject.UnitTests/StarterProject.UnitTests.csproj
+dotnet test ./src/StarterProject.InMemoryUnitTests/StarterProject.InMemoryUnitTests.csproj
+dotnet test ./src/StarterProject.IntegrationTests/StarterProject.IntegrationTests.csproj
